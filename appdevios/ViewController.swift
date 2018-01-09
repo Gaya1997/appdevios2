505d63ecb145723d6afcabadda05c96acb653283
Gayathri <Gayathri@github.com>
2018-01-09 13:23:52 +0400

    Initial commit

---
+++ appdevios/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  appdevios
+//
+//  Created by Sudheesh Kumar on 1/4/18.
+//  Copyright © 2018 Sudheesh Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Hello World")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
